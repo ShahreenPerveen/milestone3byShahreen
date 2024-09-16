dfa718f8a321b5e3c73467f726560c13ca976569
ShahreenPerveen <160607899+ShahreenPerveen@users.noreply.github.com>
2024-09-16 22:43:08 +0500

    Add files via upload

---
+++ script.ts
@@ -0,0 +1,5 @@
+export default function script() {
+    const script = document.createElement('script');
+    script.src = 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js';
+    document.body.appendChild(script);
+}
